e285562079dc70e34d3178743e6b50e6b48e1f96
medduge50 <69568693+medduge50@users.noreply.github.com>
2023-07-18 15:37:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,63 +1,39 @@
-import discord
-import asyncio
-import datetime
-import os
-import time
-import random
+from discord.ext import tasks
+import discord, datetime, pytz, os, requests, asyncio, pymysql, ctypes, sys, re
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 client = discord.Client()
 
+@client.event
+async def on_connect(): 
+    print("안상현봇")
+    game = discord.Game("응애") 
+    await client.change_presence(status=discord.Status.online, activity=game)       
 
 @client.event
-async def on_ready():
-    print("로그인")
-    print('제작자:호떡#9460')
-    print(client.user.name)
-    print(client.user.id)
-    print("---------------")
-    print('디스코드 봇이 실행되었습니다.')
+async def on_message(message):   
+    if message.content.startswith("쥐새키야 도움말"):
+        embed = discord.Embed(title=f'쥐새키 도움말', timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00FF00) 
+        embed.add_field(name='쥐새키야 내정보보내', value=f"자신의 정보를 확인할수 있습니다.", inline=False)   
+        embed.add_field(name='쥐새키야 정보알려줘 @태그', value=f"태그한사람의 정보를 확인할수 있습니다.", inline=False)      
+        embed.add_field(name='쥐새키야 욕해줘', value=f"쥐새키의 욕", inline=False)      
+        return await message.channel.send(embed=embed)  
+
+    if message.content.startswith("쥐새키야 내정보보내"):
+        embed = discord.Embed(title=f'쥐새키야 사용자 정보', description=f'```사용자 정보\n{message.author}({message.author.id})```', timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00FF00) 
+        return await message.channel.send(embed=embed)  
     
-   
-   
-@client.event
-async def on_message(message):
-    if message.content.startswith("/정보"):
-        date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="이름", value=message.author.name, inline=False)
-        embed.add_field(name="서버닉네임", value=message.author.display_name, inline=False)
-        embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일",
-                        inline=False)
-        embed.add_field(name="아이디", value=message.author.id, inline=False)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        await message.channel.send(message.channel, embed=embed)
+    if message.content.startswith("쥐새키야 욕해줘"):
+        await message.channel.send(f"니얼굴 내얼굴 ㅋ")
 
-    if message.content.startswith("/help"):
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="[help]", value="자신의 정보를 알고싶다면 '/정보'쳐주세요!", inline=False)
-        embed.add_field(name="[help]", value="/출근,/퇴근,/임시퇴근이 가능합니다!", inline=False)
-        await message.channel.send(message.channel, embed=embed)
+    if message.content.startswith("쥐새키야 정보알려줘"):
+        try:
+            user = message.mentions[0]
+        except:
+            return await message.channel.send("유저를 맨션해주세요!! 쥐새키가 화를 낼수 있습니다.\n예시 (!업로더 @맨션 사용할 이모지)")
+        embed = discord.Embed(title=f'쥐새키야 사용자 정보', description=f'```사용자 정보\n{user}({user.id})```', timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00FF00) 
+        return await message.channel.send(embed=embed)  
 
-    if message.content.startswith("/출근"):
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="[서버닉네임]", value=message.author.display_name, inline=False)
-        embed.add_field(name="[id]", value=message.author.id, inline=False)
-        embed.add_field(name="[출근]", value="출근하셨습니다.", inline=True)
-        await message.channel.send(message.channel, embed=embed)
+client.run("MTA4Njk5MzAzMzE1NDgwMTY4NQ.GNV0Ww.iGmNYKdS7veq5SZB-ueDEE2vykKSp1xjZKFBZQ")
 
-    if message.content.startswith("/퇴근"):
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="[서버닉네임]", value=message.author.display_name, inline=False)
-        embed.add_field(name="[id]", value=message.author.id, inline=False)
-        embed.add_field(name="[퇴근]", value="퇴근하셨습니다.", inline=True)
-        await message.channel.send(message.channel, embed=embed)
 
-    if message.content.startswith("/임시퇴근"):
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="[서버닉네임]", value=message.author.display_name, inline=False)
-        embed.add_field(name="[id]", value=message.author.id, inline=False)
-        embed.add_field(name="[임시퇴근]", value="임시퇴근하셨습니다.", inline=True)
-        await message.channel.send(message.channel, embed=embed)
-
-access_token = os.environ["BOT_TOKEN"]
-client.run('access_token')
